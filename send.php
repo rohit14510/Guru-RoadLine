3d17a9a2bbcf177c8840dffabdf67d24af5a2a4f
Rohit Pancholi <rohitpancholi14510@gmail.com>
2025-07-26 17:34:56 +0530

    chng

--- send.php
+++ send.php
@@ -30,7 +30,7 @@ $mail->Password = 'Standup@123';
 $mail->SMTPSecure = 'tls';
 $mail->Port     = 587;
 // Sender info 
-$mail->setFrom('info@ompackers.in', 'Om Cargo International  Packers And Movers '); 
+$mail->setFrom('info@ompackers.in', 'Guru Roadlines  '); 
 $mail->addReplyTo('info@ompackers.in', 'Test Email'); 
  
 // Add a recipient 
@@ -57,7 +57,7 @@ $mailContent = '
     <p>Service :'.$service.'</p>
     <p>Message :'.$msg.'</p> 
     <p>Thanks and Regards</p>  
-    <p>Om Cargo International  Packers And Movers </p>';  
+    <p>Guru Roadlines  </p>';  
 // $mailContent = "Name : ".$name."\n"."Subject : ".$subject."\n"."Email : ".$email."\n"."Mbile : ".$mobile."\n"."Message :".$message; 
 $mail->Body = $mailContent; 
 $mail->headers  = "From: Sender Name <standupstartups1@gmail.com>" . "\r\n";
